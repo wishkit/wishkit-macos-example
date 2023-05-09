cc331b925c6488f78d021a32849cfda1e083ba16
Martin Lasek <heylasek@gmail.com>
2023-05-08 23:02:11 -0700

    updated wishkit sdk to 3.0 / used new sdk api to configure it

--- wishkit-macos-example/ContentView.swift
+++ wishkit-macos-example/ContentView.swift
@@ -13,6 +13,21 @@ struct ContentView: View {
 
     init() {
         WishKit.configure(with: "0269EE13-3390-4003-9AEA-A69ADACFAE7C")
+
+        // Show the status badge of a wish
+        WishKit.config.statusBadge = .show
+
+        // Hide the segmented control (Requested/Implemented)
+        WishKit.config.buttons.segmentedControl.display = .hide
+
+        // Set the secondary color (this is for the cells and text fields).
+        WishKit.theme.secondaryColor = Theme.Scheme(light: .yellow.opacity(1/3), dark: .white.opacity(1/3))
+
+        // Set the tertiary color (this is for the background).
+        WishKit.theme.tertiaryColor = Theme.Scheme(light: .brown, dark: .blue)
+
+        // Change any text used by wishkit (assign localized strings to support different languages).
+        WishKit.config.localization.requested = "New"
     }
 
     var body: some View {
